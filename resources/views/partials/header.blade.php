1b17209673c908d39744c47ba9047e1d7fb59be9
Kierra Palmer <kierrapalmer@weber.edu>
2018-10-26 14:44:07 -0600

    start login

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,9 +24,22 @@
 
     </ul>
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="{{route('admin.index')}}">Admin</a>
-        </li>
+        @if(!Auth::check())
+            <li><a href="{{ url('/login') }}" class="nav-link">Login</a></li>
+            <li><a href="{{ url('/register') }}" class="nav-link">Register</a></li>
+        @else
+            <li><a href="{{ route('admin.index') }}" class="nav-link">All Items</a></li>
+            <li>
+                <a href="{{ url('/logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                   class="nav-link">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ url('/logout') }}" method="POST">
+                    {{ csrf_field() }}
+                </form>
+            </li>
+        @endif
     </ul>
 </nav>
 
